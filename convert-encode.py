c02188d7ee150a8fb3440bd2302f9a8ad4ae5a2a
Rama Almer Felix <86484270+quenzvezda@users.noreply.github.com>
2023-06-05 04:27:36 +0700

    Fix when the image is empty or broken

--- convert-encode.py
+++ convert-encode.py
@@ -2,23 +2,28 @@ import os
 import shutil
 import subprocess
 from PIL import Image
+from PIL import UnidentifiedImageError
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 def convert_image(image_path, quality, resolution):
-    img = Image.open(image_path)
+    try:
+        img = Image.open(image_path)
 
-    # If the image mode is not 'RGB', convert it to 'RGB'
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-        
-    # Preserve Aspect Ratio (height to width ratio of image is preserved)
-    img.thumbnail(resolution)
+        # If the image mode is not 'RGB', convert it to 'RGB'
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
 
-    # Save the image with reduced quality
-    new_filename = image_path.split('.')[0] + "_converted.jpg"
-    img.save(new_filename, "JPEG", optimize=True, quality=quality)
-    return new_filename
+        # Preserve Aspect Ratio (height to width ratio of image is preserved)
+        img.thumbnail(resolution)
+
+        # Save the image with reduced quality
+        new_filename = image_path.split('.')[0] + "_converted.jpg"
+        img.save(new_filename, "JPEG", optimize=True, quality=quality)
+        return new_filename
+    except UnidentifiedImageError:
+        print(f"Unidentified image error: Cannot process image {image_path}")
+        return None
 
 def compress_video(video_path, preset_file):
     output_filename = video_path.split('.')[0] + "_converted.mp4"
@@ -32,15 +37,16 @@ def batch_convert(quality, resolution, preset_file):
 
         # ensure the path is a directory
         if os.path.isdir(folder_path):
-            new_folder_path = folder_path + ' [Converted]'
+            new_folder_path = folder_path + ' [converted]'
             os.makedirs(new_folder_path, exist_ok=True)
 
             for filename in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, filename)
-                if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.webp')):
+                if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                     new_image_path = convert_image(file_path, quality, resolution)
-                    os.replace(new_image_path, os.path.join(new_folder_path, filename))
-                elif filename.lower().endswith(('.mp4', '.mov', '.avi', '.mkv', '.webm', '.flv', '.wmv', '.mkv')):
+                    if new_image_path is not None:
+                        os.replace(new_image_path, os.path.join(new_folder_path, filename))
+                elif filename.lower().endswith(('.mp4', '.mov', '.avi', '.mkv')):
                     new_video_path = compress_video(file_path, preset_file)
                     os.replace(new_video_path, os.path.join(new_folder_path, filename))
 
--- convert.py
+++ convert.py
@@ -5,36 +5,42 @@ from PIL import Image
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 def convert_image(image_path, quality, resolution):
-    img = Image.open(image_path)
-    
-    # If the image mode is not 'RGB', convert it to 'RGB'
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
+    try:
+        img = Image.open(image_path)
 
-    # Preserve Aspect Ratio (height to width ratio of image is preserved)
-    img.thumbnail(resolution)
+        # If the image mode is not 'RGB', convert it to 'RGB'
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
 
-    # Save the image with reduced quality
-    new_filename = image_path.split('.')[0] + "_converted.jpg"
-    img.save(new_filename, "JPEG", optimize=True, quality=quality)
-    return new_filename
+        # Preserve Aspect Ratio (height to width ratio of image is preserved)
+        img.thumbnail(resolution)
 
-def batch_convert(quality, resolution):
+        # Save the image with reduced quality
+        new_filename = image_path.split('.')[0] + "_converted.jpg"
+        img.save(new_filename, "JPEG", optimize=True, quality=quality)
+        return new_filename
+    except UnidentifiedImageError:
+        print(f"Unidentified image error: Cannot process image {image_path}")
+        return None
+
+def batch_convert(quality, resolution, preset_file):
     for foldername in os.listdir(current_dir):
         folder_path = os.path.join(current_dir, foldername)
 
         # ensure the path is a directory
         if os.path.isdir(folder_path):
-            new_folder_path = folder_path + ' converted'
+            new_folder_path = folder_path + ' [converted]'
             os.makedirs(new_folder_path, exist_ok=True)
 
             for filename in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, filename)
-                if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.webp')):
+                if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                     new_image_path = convert_image(file_path, quality, resolution)
-                    os.replace(new_image_path, os.path.join(new_folder_path, filename))
-                elif filename.lower().endswith(('.mp4', '.mov', '.avi','.mkv')):
-                    shutil.move(file_path, new_folder_path)
+                    if new_image_path is not None:
+                        os.replace(new_image_path, os.path.join(new_folder_path, filename))
+                elif filename.lower().endswith(('.mp4', '.mov', '.avi', '.mkv')):
+                    new_video_path = compress_video(file_path, preset_file)
+                    os.replace(new_video_path, os.path.join(new_folder_path, filename))
 
 # Change these values as needed
 quality = 90
