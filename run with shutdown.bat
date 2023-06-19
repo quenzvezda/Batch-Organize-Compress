845cbe9a3e72806aee2600c909426f6bb4e35ce5
Rama Almer Felix <86484270+quenzvezda@users.noreply.github.com>
2023-06-20 02:41:46 +0700

    add more batch script
    run with shutdown, turn off the computer when finished compressing, in seconds
    cancel shutdown, canceling computer shutdown

---
+++ run with shutdown.bat
@@ -0,0 +1,21 @@
+@echo off
+
+rem Menjalankan skrip movev3.py
+echo Menjalankan movev3.py...
+python movev3.py
+
+rem Menjalankan skrip rename.py
+echo Menjalankan rename.py...
+python rename.py
+
+rem Menjalankan skrip convert.py
+echo Menjalankan convert-encode.py...
+python convert-encode.py
+
+rem Menampilkan pesan selesai
+echo Semua Skrip Selesai dijalankan.
+shutdown.exe /s /t 300
+pause >nul
+
+rem Menutup jendela Command Prompt
+exit
